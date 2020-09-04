b76138974431dac53cd383359a3dfde6499f89ea
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2020-09-04 12:55:26 -0300

    video 1.4

---
+++ src/pages/Pagina404/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Page404 = () => <h1>Essa página não existe :(</h1>;
+
+export default Page404;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Board from '../pages/Board';
+import Pagina404 from '../pages/Pagina404';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/board" component={Board} />
+      <Route path="*" component={Pagina404} />
     </Switch>
   </BrowserRouter>
 );
